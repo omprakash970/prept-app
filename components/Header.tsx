010c7e1f3e21733d7c7fee50f150e4a9b73781e9
Bandi Om Prakash <omprakashbandi583@gmail.com>
2026-05-04 20:52:05 +0530

    done

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,48 +1,41 @@
 'use client'
 
 import { SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
-import { useUser } from '@clerk/nextjs'
-import Image from 'next/image'
 import React from 'react'
 import { Button } from './ui/button'
+import { Show } from '@clerk/react'
 import Link from 'next/link'
-
+import Image from 'next/image'
 
 const Header = () => {
-  const { user } = useUser()
-
   return (
     <nav className='fixed top-0 inset-x-0 z-50 flex items-center justify-between px-10 py-3 border-b border-white/7 backdrop-blur-xl'>
       
-      {/* Logo */}
-      <div>
-        <Link href="/">
-          <Image 
-            src="/logo.png" 
-            alt="logo" 
-            width={100} 
-            height={100} 
-            className="w-auto h-10"
-          />
-        </Link>
-      </div>
-
-      {/* Right Side */}
+     <Link href='/'>
+       <Image src={'/logo.png'} alt='Prept Logo' width={70} height={60}  />
+     </Link>
       <div className='flex items-center gap-3'>
+   
+      
         
-        {!user ? (
-          <>
-            <SignInButton mode="modal">
-              <Button variant="ghost">Sign In</Button>
-            </SignInButton>
-
-            <SignUpButton>
-              <Button variant="ghost">Get Started</Button>
-            </SignUpButton>
-          </>
-        ) : (
-          <UserButton />
-        )}
+        <Show when={"signed-out"}>
+          <SignInButton mode='modal'>
+            <Button variant="ghost">
+              Sign In
+            </Button>
+          </SignInButton>
+          <SignUpButton >
+            <Button variant="ghost">
+              Get Started
+            </Button>
+          </SignUpButton>
+        </Show>
+      <Show when={"signed-in"}>
+        <UserButton />
+      </Show>
+       
+        
+       
 
       </div>
       
