dd0d74bc865b8eac214770649552c0e6448d61e9
Bandi Om Prakash <omprakashbandi583@gmail.com>
2026-05-05 20:34:18 +0530

    done

--- app/page.jsx
+++ app/page.jsx
@@ -2,12 +2,15 @@ import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import { Car } from "lucide-react";
+import { GoldTitle, GrayTitle } from "@/components/resusables";
 export default function Home() {
   return (
     <>
     <section className="pt-28 sm:pt-32">
 
      <Button  variant={"gold"}>Subsrcibe</Button>
+     <GrayTitle >Gray Title</GrayTitle>
+     <GoldTitle>Gold Title</GoldTitle>
     </section>
 
     
---
+++ components/resusables.jsx
@@ -0,0 +1,3 @@
+export const GrayTitle = ({children})=><span className="bg-gradient-to-br from-stone-100 via-stone-300 to-stone-500">{children}</span>
+export const GoldTitle = ({children})=><span className="bg-gradient-to-br from-amber-100 via-amber-300 to-amber-500">{children}</span>
+export const SectionLable =({children})=><span className="bg-gradient-to-br from-slate-100 via-slate-300 to-slate-500">{children}</span>
